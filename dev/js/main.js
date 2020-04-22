da0d9fe24262cd068eef5bb83e1734ec281cd306
milanbargiel <milan.bargiel@googlemail.com>
2020-04-23 00:17:14 +0200

    add mailchimp form

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,4 +1,10 @@
-// document.querySelector('.js-submit').addEventListener('click', (event) => {
+// const form = document.querySelector('.js-form');
+
+// form.querySelector('.js-submit').addEventListener('click', (event) => {
 //   event.preventDefault(); // prevent form submit and required popups
-//   console.log('I was clicked');
+//   const invalidElements = form.querySelector(':invalid');
+//   console.log(invalidElements.length);
+
+//   // if (form.querySelector(':invalid')) { // invalid input
+//   // }
 // });
