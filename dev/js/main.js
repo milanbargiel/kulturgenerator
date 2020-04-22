1dbd935bf83764bb8c6f55fa879428f94088cab5
milanbargiel <milan.bargiel@googlemail.com>
2020-04-22 21:34:03 +0200

    add favicon

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,1 +1,4 @@
-console.log('kokos bananae');
+// document.querySelector('.js-submit').addEventListener('click', (event) => {
+//   event.preventDefault(); // prevent form submit and required popups
+//   console.log('I was clicked');
+// });
