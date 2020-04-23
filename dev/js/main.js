a9afd30f351792e041c376bb0135b2168db6ad43
milanbargiel <milan.bargiel@googlemail.com>
2020-04-23 22:49:35 +0200

    add basic ajax form submission

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,10 +1,50 @@
-// const form = document.querySelector('.js-form');
+const newsletterForm = document.querySelector('.js-form');
+const emailInput = newsletterForm.querySelector('.js-e-mail');
+const successText = document.querySelector('.js-success-text');
+const errorText = document.querySelector('.js-error-text');
 
-// form.querySelector('.js-submit').addEventListener('click', (event) => {
-//   event.preventDefault(); // prevent form submit and required popups
-//   const invalidElements = form.querySelector(':invalid');
-//   console.log(invalidElements.length);
+function serialize(inputField) {
+  const serialized = `&${encodeURIComponent(inputField.name)}=${encodeURIComponent(inputField.value)}`;
+  return serialized;
+}
 
-//   // if (form.querySelector(':invalid')) { // invalid input
-//   // }
-// });
+function submitMailChimpForm(form, inputField) {
+  let url = form.getAttribute('action');
+  url = url.replace('/post?u=', '/post-json?u=');
+  url += `${serialize(inputField)}&c=displayMailChimpStatus`;
+
+  // Create and load script for ajax with url and callback when form is submitted
+  const script = window.document.createElement('script');
+  script.src = url;
+
+  // Insert script tag into the DOM (append to <head>)
+  document.head.appendChild(script);
+
+  // After the script is loaded (and executed), remove it
+  /* eslint-disable func-names */
+  script.onload = function () {
+    this.remove();
+  };
+}
+
+newsletterForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  // clear form responses
+  successText.style.display = 'none';
+  errorText.style.display = 'none';
+  // submit form
+  submitMailChimpForm(event.target, emailInput);
+});
+
+/* eslint-disable no-unused-vars */
+function displayMailChimpStatus(data) {
+  // for debugging only
+  // console.log(data);
+  if (data.result === 'error') {
+    errorText.style.display = 'block';
+    return;
+  }
+  // on success
+  newsletterForm.reset(); // clear input
+  successText.style.display = 'block';
+}
