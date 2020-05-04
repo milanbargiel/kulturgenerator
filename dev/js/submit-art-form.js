709cd97fcaea369aad6af76b38acd23b1dde751b
milanbargiel <milan.bargiel@googlemail.com>
2020-05-04 12:45:01 +0200

    show selected form on click

--- dev/js/submit-art-form.js
+++ dev/js/submit-art-form.js
@@ -1,3 +1,22 @@
+const categoryButtons = document.querySelectorAll('.js-category-selection');
+const submitArtForms = document.querySelectorAll('.js-form');
+
+// show selected form onclick
+categoryButtons.forEach((button) => {
+  button.addEventListener('change', () => {
+    // hide all forms except chosen form
+    submitArtForms.forEach((element) => {
+      const form = element;
+      if (form.className.includes(button.value)) {
+        form.style.display = 'block';
+      } else {
+        form.style.display = 'none';
+      }
+    });
+  });
+});
+
+
 const fileInput = document.querySelector('.js-file-input');
 const hiddenImageData = document.querySelector('.js-hidden-image-data');
 
