2ae8fc67b722ddaa2b9882f5cf2e413dc8a71895
milanbargiel <milan.bargiel@googlemail.com>
2020-05-04 16:44:21 +0200

    add basic styled image upload

--- dev/js/submit-art-form.js
+++ dev/js/submit-art-form.js
@@ -16,9 +16,12 @@ categoryButtons.forEach((button) => {
   });
 });
 
+// document.querySelector('.js-form-gut').style.display = 'block'; // for dev purpose
+
 
 const fileInput = document.querySelector('.js-file-input');
 const hiddenImageData = document.querySelector('.js-hidden-image-data');
+const fileContainer = document.querySelector('.js-file-container');
 
 // listen to change on input type file
 fileInput.addEventListener('change', (inputEvent) => {
@@ -30,9 +33,25 @@ fileInput.addEventListener('change', (inputEvent) => {
     let html = `<input type="hidden" name="data" value="${readerEvent.target.result.replace(/^.*,/, '')}" >`;
     html += `<input type="hidden" name="mimetype" value="${readerEvent.target.result.match(/^.*(?=;)/)[0]}" >`;
     hiddenImageData.innerHTML = html;
+    // display image in file container
+    fileContainer.innerHTML = `<div class="iu__file">${file.name}<span class="iu__file-remove js-file-remove">x</span></div>`;
+    fileContainer.style.border = '1px solid black';
+    fileContainer.style.borderTopColor = '#fff';
   });
 
   if (file) {
     reader.readAsDataURL(file);
+  } else { // no file selected
+    hiddenImageData.innerHTML = '';
+    fileContainer.innerHTML = '';
+    fileContainer.style.border = 'none';
+  }
+});
+
+// remove file selection on user wish
+fileContainer.addEventListener('click', (event) => {
+  if (event.target && event.target.className.includes('js-file-remove')) {
+    fileInput.value = null;
+    fileInput.dispatchEvent(new Event('change'));
   }
 });
