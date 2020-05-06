5d049bd1f4041e4314f6d42cb2820551d553b708
milanbargiel <milan.bargiel@googlemail.com>
2020-05-06 11:25:37 +0200

    add show selected form function

--- dev/js/submit-art-form.js
+++ dev/js/submit-art-form.js
@@ -2,28 +2,29 @@
 const categoryRadioButtons = document.querySelectorAll('.js-category-input');
 const submitArtForms = document.querySelectorAll('.js-form');
 
+function showSelectedForm(forms, selector) {
+  forms.forEach((form) => {
+    // clear visibility classes for explain texts
+    form.classList.remove('sa-form--objekt');
+    form.classList.remove('sa-form--erlebnis');
+    form.classList.remove('sa-form--motiv');
+
+    // show form when selected
+    if (form.className.includes(selector)) {
+      form.setAttribute('style', 'display:block;');
+      form.classList.add(`sa-form--${selector}`);
+    } else {
+      form.removeAttribute('style');
+    }
+  });
+}
+
 // show selected form onclick
 categoryRadioButtons.forEach((radioButton) => {
-  const radio = radioButton;
-
-  radio.addEventListener('change', () => {
-    submitArtForms.forEach((element) => {
-      const form = element;
-
-      // clear visibility classes for explain texts
-      form.classList.remove('sa-form--objekt');
-      form.classList.remove('sa-form--erlebnis');
-      form.classList.remove('sa-form--motiv');
-
-      // show form when selected
-      if (form.className.includes(radio.value)) {
-        form.style.display = 'block';
-        form.classList.add(`sa-form--${radio.value}`);
-      } else {
-        form.style.display = 'none';
-      }
-    });
+  if (radioButton.checked) { // for dev purpose
+    showSelectedForm(submitArtForms, radioButton.value);
+  }
+  radioButton.addEventListener('change', () => {
+    showSelectedForm(submitArtForms, radioButton.value);
   });
 });
-
-// document.querySelector('.js-form-gut').style.display = 'block'; // for dev purpose
