fbcf036b070973a514d8b13d0a233ab3a72a95d7
milanbargiel <milan.bargiel@googlemail.com>
2020-05-06 18:15:08 +0200

    make footer smaller

--- dev/js/submit-art-form.js
+++ dev/js/submit-art-form.js
@@ -21,9 +21,9 @@ function showSelectedForm(forms, selector) {
 
 // show selected form onclick
 categoryRadioButtons.forEach((radioButton) => {
-  if (radioButton.checked) { // for dev purpose
-    showSelectedForm(submitArtForms, radioButton.value);
-  }
+  // if (radioButton.checked) { // for dev purpose
+  //   showSelectedForm(submitArtForms, radioButton.value);
+  // }
   radioButton.addEventListener('change', () => {
     showSelectedForm(submitArtForms, radioButton.value);
   });
