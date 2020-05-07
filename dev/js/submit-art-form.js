d426314900dbe7f517eab99a1e6b6a94ae87aa54
milanbargiel <milan.bargiel@googlemail.com>
2020-05-07 13:19:52 +0200

    add clear Form function to reset form on category selection

--- dev/js/submit-art-form.js
+++ dev/js/submit-art-form.js
@@ -1,8 +1,13 @@
 // categories
 const categoryRadioButtons = document.querySelectorAll('.js-category-input');
-const submitArtForms = document.querySelectorAll('.js-form');
+const submitArtForm = document.querySelector('.js-form');
 const selectedCategoryInput = document.querySelector('.js-selected-category');
 const allRequiredInputs = document.querySelectorAll('.js-required-input');
+
+// 100 percent feature
+const hundertPercent = document.querySelector('.js-100-percent-input');
+const hundertPercentThankYou = document.querySelector('.js-thank-you-100-percent');
+const hundertPercentToggles = document.querySelectorAll('.js-100-percent-toggle-visibility');
 
 function isVisible(el) {
   return !(el.offsetParent === null);
@@ -18,27 +23,34 @@ function makeVisibleInputsRequired() {
   });
 }
 
+function clearForm() {
+  // reset form
+  submitArtForm.reset();
+  // manually dispatch change event on hunderPercentDonation listener
+  // to reset visibility toggles
+  const event = document.createEvent('HTMLEvents');
+  event.initEvent('change', false, true);
+  hundertPercent.dispatchEvent(event);
+}
+
 function showSelectedForm(selector) {
+  // clear previous form inputs
+  clearForm();
+
   // set hidden input to category name
   selectedCategoryInput.value = selector;
 
+  // clear visibility classes for explain texts
+  submitArtForm.classList.remove('sa-form--objekt');
+  submitArtForm.classList.remove('sa-form--erlebnis');
+  submitArtForm.classList.remove('sa-form--motiv');
+
   // show selected funnel
-  submitArtForms.forEach((form) => {
-    // clear visibility classes for explain texts
-    form.classList.remove('sa-form--objekt');
-    form.classList.remove('sa-form--erlebnis');
-    form.classList.remove('sa-form--motiv');
+  if (submitArtForm.className.includes(selector)) {
+    submitArtForm.setAttribute('style', 'display:block;');
+    submitArtForm.classList.add(`sa-form--${selector}`);
+  }
 
-    // show form when selected
-    if (form.className.includes(selector)) {
-      form.setAttribute('style', 'display:block;');
-      form.classList.add(`sa-form--${selector}`);
-    } else {
-      form.removeAttribute('style');
-    }
-  });
-
-  // set html required attribute for visible inputs
   makeVisibleInputsRequired();
 }
 
@@ -52,10 +64,6 @@ categoryRadioButtons.forEach((radioButton) => {
     showSelectedForm(radioButton.value);
   });
 });
-
-const hundertPercent = document.querySelector('.js-100-percent-input');
-const hundertPercentThankYou = document.querySelector('.js-thank-you-100-percent');
-const hundertPercentToggles = document.querySelectorAll('.js-100-percent-toggle-visibility');
 
 hundertPercent.addEventListener('change', (event) => {
   if (event.target.checked) {
