71193e395d8d112eb710c5204da9761fe50c54b2
milanbargiel <milan.bargiel@googlemail.com>
2020-08-12 16:17:36 +0200

    Make name dropping banner pure JS for cross browser compability

--- landingpage/dev/js/log.js
+++ landingpage/dev/js/log.js
@@ -23,3 +23,11 @@ let names = [
 names = names.sort(() => Math.random() - 0.5); // shuffle names
 names = `◰◱◳ Mit ${names.join(' - ')} ◰◱◳`; // join them
 container.innerHTML = names; // paste to assembly line bar
+
+$('.marquee').marquee({
+  duration: 15000,
+  gap: 10,
+  delayBeforeStart: 0,
+  direction: 'left',
+  duplicated: true,
+});
