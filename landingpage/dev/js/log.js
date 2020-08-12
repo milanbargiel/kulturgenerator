e110c72c8e0a5011e864e14a768ac9d45d432d30
milanbargiel <milan.bargiel@googlemail.com>
2020-08-12 13:02:52 +0200

    Add name dropping banner

---
+++ landingpage/dev/js/log.js
@@ -0,0 +1,25 @@
+const container = document.querySelector('.js-names-container');
+
+let names = [
+  'Thomas Kemper',
+  'Ute Wegmann',
+  'Andreas Tritsch',
+  'Gerlis Zillgens',
+  'Tobias Schulenburg',
+  'Nadjana Mohr',
+  'Anke von Claer-Schaar',
+  'Johannes Specks',
+  'Maurits Boettger',
+  'Andreas Keil',
+  'Sebastian Osterhaus',
+  'Beate Gördes',
+  'Kriz Olbricht',
+  'Brigitte Dunkel',
+  'Dagmar Hugk',
+  'Dorian Steinhoff',
+  'Tobias Schulenburg',
+];
+
+names = names.sort(() => Math.random() - 0.5); // shuffle names
+names = `◰◱◳ Mit ${names.join(' - ')} ◰◱◳`; // join them
+container.innerHTML = names; // paste to assembly line bar
