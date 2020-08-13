305c324d0400effd80ad6483a3de354ee6db876f
Moritz Pflüger <m.pflueger@meta-fusion.com>
2020-08-13 16:10:53 +0200

    initiate strapi cms app

---
+++ kulturgenerator-cms/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '23688ebfcf213b157f795cc76dbc6919'),
+    },
+  },
+});
---
+++ kulturgenerator-cms/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e03bfabb-3ae4-42a8-889d-8fb431eb0348'
+};
