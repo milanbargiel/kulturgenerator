a4695509793ab123bae29ccfeb87271987b10c42
Moritz Pflüger <m.pflueger@meta-fusion.com>
2020-08-21 17:20:12 +0200

    add production Api BASE_URL (testing only)

--- kulturschleuder/src/components/RestaurantList.vue
+++ kulturschleuder/src/components/RestaurantList.vue
@@ -23,7 +23,8 @@ export default {
     }
   },
   created () {
-    axios.get('http://localhost:1337/restaurants')
+    // axios.get('http://localhost:1337/restaurants') // use for local environment TODO: implement .env file locally and on the server
+    axios.get('https://xyz.kulturgenerator.de/restaurants') // use for production environment TODO: implement .env file locally and on the server
       .then(response => {
         this.restaurants = response.data
       })
