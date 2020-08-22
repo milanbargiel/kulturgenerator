b7f83430ea3156a060cc0e3af055413b21d2f3af
Moritz Pflüger <m.pflueger@meta-fusion.com>
2020-08-22 12:22:50 +0200

    [App] make api url an environment variable

--- kulturschleuder/src/components/RestaurantList.vue
+++ kulturschleuder/src/components/RestaurantList.vue
@@ -23,8 +23,7 @@ export default {
     }
   },
   created () {
-    // axios.get('http://localhost:1337/restaurants') // use for local environment TODO: implement .env file locally and on the server
-    axios.get('https://xyz.kulturgenerator.de/restaurants') // use for production environment TODO: implement .env file locally and on the server
+    axios.get(process.env.VUE_APP_API_BASEURL + '/restaurants')
       .then(response => {
         this.restaurants = response.data
       })
