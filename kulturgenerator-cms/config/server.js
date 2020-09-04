ff845597993b007861947c8a822d444e1f123ef3
Moritz Pflüger <m.pflueger@meta-fusion.com>
2020-09-04 17:27:46 +0200

    add url to strapi config server.js

--- kulturgenerator-cms/config/server.js
+++ kulturgenerator-cms/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'xyz.kulturgenerator.de',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '32868dff0a05250d9edf538ac624f630'),
