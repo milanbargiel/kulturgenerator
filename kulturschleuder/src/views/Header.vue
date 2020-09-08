20ef677a0a271d40a628a9e547e34f7aa5a79447
milanbargiel <milan.bargiel@googlemail.com>
2020-09-08 08:28:39 +0200

    [App] Use text-arrow instead of computed property for back button

--- kulturschleuder/src/views/Header.vue
+++ kulturschleuder/src/views/Header.vue
@@ -5,7 +5,7 @@
                 <marquee-text
                     :repeat="20"
                     :duration="5">
-                    <span class="header__back-arrow-icon" v-html="backArrowIcon"></span>
+                    <span class="header__back-arrow-icon">←</span>
                 </marquee-text>            
             </router-link>            
         </div>
@@ -23,9 +23,6 @@ export default {
     computed: {
         showBackButton () {
             return this.$route.meta.showBackButton || false
-        },
-        backArrowIcon () {
-            return "&#129120;" // TODO: exchange arrow icon
         }
     }
 }
