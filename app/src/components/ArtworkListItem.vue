5cc164991a8ccb9f2f601601fa8b56361914ae74
milanbargiel <milan.bargiel@googlemail.com>
2020-09-30 12:51:35 +0200

    [App] Add responsive image variants for retina displays

--- app/src/components/ArtworkListItem.vue
+++ app/src/components/ArtworkListItem.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="artwork-list-item" :style="styles">
-        <img class="artwork-list-item__image" v-lazy="imgUrl">
+        <img class="artwork-list-item__image" :v-lazy="imgUrl" :srcset="srcSet">
         <router-link class="artwork-list-item__link" :to="{ name: 'artworkDetail', params: { id: item.id, author: item.author, title: item.title }}">
             <span class="artwork-list-item__author">{{ item.author }}: </span>
             <span class="artwork-list-item__title">{{ item.title }}</span>, 
@@ -13,9 +13,33 @@
 export default {
     name: 'ArtworkListItem',
     props: ['item'],
+    methods: {
+        imgVariant (breakpoint) {
+            const img = this.item.images[0]
+            let url = img.url // unresized image url
+            
+            // return resized url for breakpoint if present
+            for (const format in img.formats) {
+                if (format === breakpoint && img.formats[breakpoint]) { 
+                    url = img.formats[breakpoint].url
+                }
+            }
+
+            return url
+        }
+    },
     computed: {
         imgUrl () {
-            return process.env.VUE_APP_API_BASEURL + this.item.images[0].url
+            // Get 'large' variant of image, if existent
+            // If not, take the unresized one
+            return process.env.VUE_APP_API_BASEURL + this.imgVariant('large')
+        },
+        srcSet () {
+            // Create srcset attribute for responsive images
+            const regularImg = `${process.env.VUE_APP_API_BASEURL + this.imgVariant('large')} 1x,`
+            const retinaImg = `${process.env.VUE_APP_API_BASEURL + this.imgVariant('x2')} 2x`
+
+            return regularImg + retinaImg
         },
         styles () {
             const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
