c28b2e826a8faff18db1f4aec92bf3edfb19c37e
milanbargiel <milan.bargiel@googlemail.com>
2020-10-02 09:18:03 +0200

    [CMS] Introduce custom order controller

--- cms/api/order/controllers/order.js
+++ cms/api/order/controllers/order.js
@@ -1,8 +1,20 @@
-'use strict';
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
 
-module.exports = {};
+  async create(ctx) {
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.order.create(data, { files });
+    } else {
+      entity = await strapi.services.order.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.order });
+  },
+};
