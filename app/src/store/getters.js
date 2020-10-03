2c5ff7de4d58fceb2de646d8cfbcd67f744ab991
Moritz Pflüger <m.pflueger@meta-fusion.com>
2020-10-03 09:24:44 +0200

    [App] refactor artworks getter sorting by quantity but also keeping random sort

--- app/src/store/getters.js
+++ app/src/store/getters.js
@@ -6,23 +6,11 @@ const getters = {
         return state.artworks[id]
     },
     getRandomizedArtworks: state => {
-        const active = []; // artworks that can be bought
-        const archive = []; // already sold artworks
-        
-        // randomize
-        Object.values(state.artworks)
-            .map((a) => ({sort: Math.random(), value: a}))
-            .sort((a, b) => a.sort - b.sort)
-            .map((a) => {
-                if (a.value.quantity < 1) { // sold
-                    archive.push(a.value)
-                } else {
-                    active.push(a.value)
-                }
-            });
-
-        // display sold artworks last
-        return active.concat(archive)
+        return Object.values(state.artworks)
+            .map(item => ({ sort: Math.random(), value: item })) // introduce random sort parameter
+            .sort((a, b) => a.sort - b.sort) // sort by random sort parameter
+            .map(item => item.value) // exclude random sort parameter
+            .sort(a => a.quantity > 0 ? -1 : 1) // move sold out items to the back
     }
 }
 
