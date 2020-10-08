92ebc504fb8219c7ddb93f7702f268e86729140d
milanbargiel <milan.bargiel@googlemail.com>
2020-10-08 11:43:23 +0200

    [CMS] Use mail templates

--- cms/api/order/controllers/order.js
+++ cms/api/order/controllers/order.js
@@ -1,4 +1,10 @@
 const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+const Email = require('email-templates');
+
+const renderMail = (entry, templateFolder) => {
+  const email = new Email();
+  return email.renderAll(`../mails/${templateFolder}`, { entry });
+};
 
 module.exports = {
   /**
@@ -19,16 +25,17 @@ module.exports = {
     
     const entry = sanitizeEntity(entity, { model: strapi.models.order });
 
-    console.log(entry);
-
     if (entry) {
+      // Pass entry data to templates
+      const buyerMail = await renderMail(entry, 'artwork-purchased');
+      const artistMail = await renderMail(entry, 'artwork-sold');
+      
       // Send E-Mail to buyer
       await strapi.plugins['email'].services.email.send({
         to: entry.buyerEmail,
         from: 'info@kulturgenerator.de',
         subject: 'Vielen Dank für Ihren Einkauf',
-        text: `Hallo ${entry.buyerFullname},\r\n\r\nSie haben ein Kunstwerk über den Kulturgenerator gekauft. Vielen Dank dafür!\r\n\r\nDie Künstlerin bzw. der Künstler wird sich bei Ihnen melden, um die Übergabe oder den Versand des Werkes zu besprechen.\r\n\r\n${entry.orderQuantity} x ${entry.artwork.author}: ${entry.artwork.title}\r\n\r\nBei Fragen wenden Sie sich gerne an info@kulturgenerator.de.\r\n\r\nFreundliche Grüße\r\nIhr Team des Kulturgenerators`,
-        html: `Hallo ${entry.buyerFullname},<br><br>Sie haben ein Kunstwerk über den Kulturgenerator gekauft. Vielen Dank dafür!<br><br>Die Künstlerin bzw. der Künstler wird sich bei Ihnen melden, um die Übergabe oder den Versand des Werkes zu besprechen.<br><br><i>${entry.orderQuantity} x ${entry.artwork.author}: ${entry.artwork.title}</i><br><br>Bei Fragen wenden Sie sich gerne an info@kulturgenerator.de.<br><br>Freundliche Grüße<br>Ihr Team des Kulturgenerators`
+        text: buyerMail.text
       });
 
       // Send E-Mail to artist
@@ -36,8 +43,7 @@ module.exports = {
         to: entry.artwork.email,
         from: 'info@kulturgenerator.de',
         subject: 'Ihr Kunstwerk wurde verkauft',
-        text: `Hallo ${entry.artwork.author},\r\n\r\nsoeben wurde Ihr Kunstwerk über den Kulturgenerator verkauft, vielen Dank für Ihre Einreichung.\r\n\r\n${entry.orderQuantity} x ${entry.artwork.author}: ${entry.artwork.title}\r\n\r\nBitte schicken Sie das Kunstwerk an folgende Adresse und nehmen Sie Kontakt zur Käuferin bzw. zum Käufer zu Vereinbarung der Übergabe auf:\r\n\r\n${entry.buyerFullname}\r\n${entry.buyerEmail}\r\n${entry.buyerStreet}\r\n${entry.buyerCity}\r\n${entry.buyerState}\r\n${entry.buyerZipCode}\r\n\r\nBei Fragen zum weiteren Vorghehen wenden Sie sich gerne an info@kulturgenerator.de.\r\n\r\nFreundliche Grüße\r\nIhr Team des Kulturgenerators`,
-        html: `Hallo ${entry.artwork.author},<br><br>soeben wurde Ihr Kunstwerk über den Kulturgenerator verkauft, vielen Dank für Ihre Einreichung.<br><br><i>${entry.orderQuantity} x ${entry.artwork.author}: ${entry.artwork.title}</i><br><br>Bitte schicken Sie das Kunstwerk an folgende Adresse und nehmen Sie Kontakt zur Käuferin bzw. zum Käufer zu Vereinbarung der Übergabe auf:<br><br>${entry.buyerFullname}<br>${entry.buyerEmail}<br>${entry.buyerStreet}<br>${entry.buyerCity}<br>${entry.buyerState}<br>${entry.buyerZipCode}<br><br>Bei Fragen zum weiteren Vorghehen wenden Sie sich gerne an info@kulturgenerator.de.<br><br>Freundliche Grüße<br>Ihr Team des Kulturgenerators`,
+        text: artistMail.text
       });
     }
 
