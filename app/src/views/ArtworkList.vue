93bc761c7414c86e1ccc43b0aacf0f49e4c9a69d
milanbargiel <milan.bargiel@googlemail.com>
2020-10-19 16:14:00 +0200

    [App] Add basic footer for shop view

--- app/src/views/ArtworkList.vue
+++ app/src/views/ArtworkList.vue
@@ -1,19 +1,22 @@
 <template>
-  <div @click="addItem()">
+  <div>
     <div class="artwork-list">
-        <artwork-list-item v-for="artwork in artworks" :key="artwork.id" :item="artwork"></artwork-list-item>
+      <artwork-list-item v-for="artwork in artworks" :key="artwork.id" :item="artwork"></artwork-list-item>
     </div>
+    <my-footer></my-footer>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 import ArtworkListItem from '../components/ArtworkListItem.vue'
+import MyFooter from '../components/Footer'
 
 export default {
   name: 'ArtworkList',
   components: {
-      ArtworkListItem
+    ArtworkListItem,
+    MyFooter
   },
   data () {
     return {
@@ -30,8 +33,8 @@ export default {
   },
   created () {
     if (this.artworks.length <= 1) {
-    this.$store.commit('SET_LOADING_STATE', true)      
-    this.$store.dispatch('getArtworks')
+      this.$store.commit('SET_LOADING_STATE', true)      
+      this.$store.dispatch('getArtworks')
       .then(() => this.$store.commit('SET_LOADING_STATE', false))
     }
   },
