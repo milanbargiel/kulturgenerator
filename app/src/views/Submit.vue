312c23b817897701aa60ad2749fe09ef567602bc
milanbargiel <milan.bargiel@googlemail.com>
2020-10-20 14:38:37 +0200

    [App] Add basic layout for submit page

--- app/src/views/Submit.vue
+++ app/src/views/Submit.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="intro">
-    Der <span class="heading">kulturgenerator</span> ist virtueller Laden und Kunstprojekt, generöse Maschine und Startschuss für eine Plattform, die spartenübergreifend die Facetten der Kölner Kulturszene abbildet. Er ist ein spieltheoretisches Experiment, in dem die Kulturschaffenden selbst entscheiden, ob und wie viel sie vom Erlös des Verkaufs ihrer Arbeit in ein solidarisches Konto einzahlen möchten. Unser Ziel ist es, im Generator eingenommenes Geld unter allen Beteiligten fair zu verteilen. Als Kulturschaffende*r, Künstler*in oder Institution können Sie nun künstlerische Werke, kulturelle Erlebnisse und Motive für Textildrucke einreichen. Für mehr Informationen lesen Sie bitte das <a class="underlined-link underlined-link--blue" href="faq.html">FAQ</a>.
+    <div class="content">
+      <div class="intro">
+            Der <span class="heading">kulturgenerator</span> ist virtueller Laden und Kunstprojekt, generöse Maschine und Startschuss für eine Plattform, die spartenübergreifend die Facetten der Kölner Kulturszene abbildet. Er ist ein spieltheoretisches Experiment, in dem die Kulturschaffenden selbst entscheiden, ob und wie viel sie vom Erlös des Verkaufs ihrer Arbeit in ein solidarisches Konto einzahlen möchten. Unser Ziel ist es, im Generator eingenommenes Geld unter allen Beteiligten fair zu verteilen. Als Kulturschaffende*r, Künstler*in oder Institution können Sie nun künstlerische Werke, kulturelle Erlebnisse und Motive für Textildrucke einreichen. Für mehr Informationen lesen Sie bitte das <a class="underlined-link underlined-link--blue" href="faq.html">FAQ</a>.
+        </div>
     </div>
 </template>
 
@@ -9,6 +11,6 @@ export default {
   name: 'Submit',
   created () {
     this.$store.commit('SET_LOADING_STATE', false)
-  }
+}
 }
 </script>
