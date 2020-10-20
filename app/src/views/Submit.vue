51d63dfc8c99141debe128b84891df3634c4ae6a
milanbargiel <milan.bargiel@googlemail.com>
2020-10-20 15:11:39 +0200

    [App] Add footer to submit page

--- app/src/views/Submit.vue
+++ app/src/views/Submit.vue
@@ -3,12 +3,18 @@
       <div class="intro">
             Der <span class="heading">kulturgenerator</span> ist virtueller Laden und Kunstprojekt, generöse Maschine und Startschuss für eine Plattform, die spartenübergreifend die Facetten der Kölner Kulturszene abbildet. Er ist ein spieltheoretisches Experiment, in dem die Kulturschaffenden selbst entscheiden, ob und wie viel sie vom Erlös des Verkaufs ihrer Arbeit in ein solidarisches Konto einzahlen möchten. Unser Ziel ist es, im Generator eingenommenes Geld unter allen Beteiligten fair zu verteilen. Als Kulturschaffende*r, Künstler*in oder Institution können Sie nun künstlerische Werke, kulturelle Erlebnisse und Motive für Textildrucke einreichen. Für mehr Informationen lesen Sie bitte das <a class="underlined-link underlined-link--blue" href="faq.html">FAQ</a>.
         </div>
+        <my-footer></my-footer>
     </div>
 </template>
 
 <script>
+import MyFooter from '../components/Footer'
+
 export default {
   name: 'Submit',
+  components: {
+    MyFooter
+  },
   created () {
     this.$store.commit('SET_LOADING_STATE', false)
 }
