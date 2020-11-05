6e77fec2d0df740890bb12b83028d5143919c1b2
milanbargiel <milan.bargiel@googlemail.com>
2020-11-05 19:07:39 +0100

    [App] Sticky header

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="header">
-        <template v-for="(menuItem, index) in menuItems">
-            <menu-item :item="menuItem" :key="'menu-item-' + index"></menu-item>
-        </template>
-        <payment-feedback-banner v-if="paymentFeedback.show" :state="paymentFeedback.state"></payment-feedback-banner>          
-    </div>
+  <div ref="header" class="header">
+    <template v-for="(menuItem, index) in menuItems">
+      <menu-item :item="menuItem" :key="'menu-item-' + index" :class="{ 'sticky': isSticky }"></menu-item>
+    </template>
+    <payment-feedback-banner v-if="paymentFeedback.show" :state="paymentFeedback.state"></payment-feedback-banner>          
+  </div>
 </template>
 
 <script>
@@ -12,15 +12,49 @@ import MenuItem from '../components/MenuItem'
 import PaymentFeedbackBanner from '../components/PaymentFeedbackBanner'
 
 export default {
-    name: 'Header',
-    components: { MenuItem, PaymentFeedbackBanner },
-    computed: {
-        menuItems () {
-            return this.$store.state.menuItems
-        },
-        paymentFeedback () {
-            return this.$store.state.paymentFeedback
-        }
+  name: 'Header',
+  components: { MenuItem, PaymentFeedbackBanner },
+  data() {
+    return {
+      isSticky: false,
+      scrollPosition: 0,
+      headerPosition: 0
     }
+  },
+  created () {
+    window.addEventListener('scroll', this.handleScroll);
+  },
+  destroyed () {
+    window.removeEventListener('scroll', this.handleScroll);
+  },
+  methods: {
+    handleScroll() {
+      if (!this.isArtworkList || this.viewportWidth < 680) {
+        return;
+      }
+      this.headerPosition = this.$refs.header.offsetTop
+      this.scrollPosition = window.scrollY
+
+      if (this.scrollPosition > this.headerPosition){
+        this.isSticky = true
+      } else {
+        this.isSticky = false
+      }
+    }
+  },
+  computed: {
+    menuItems () {
+      return this.$store.state.menuItems
+    },
+    paymentFeedback () {
+      return this.$store.state.paymentFeedback
+    },
+    isArtworkList() {
+      return (this.$route.name === 'artworkList') || false
+    },
+    viewportWidth () {
+      return Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+    },
+  }
 }
 </script>
