8e1349cc11808eaa576a8bc4d07aa39707f8e8d7
milanbargiel <milan.bargiel@googlemail.com>
2020-11-07 12:20:35 +0100

    [App] Fix indentation

--- app/src/components/Newsletter.vue
+++ app/src/components/Newsletter.vue
@@ -6,22 +6,22 @@
   @error="onError"
   @success="onSuccess"
   >
-  <template v-slot="{ subscribe, setEmail, error, success, loading }">
-    <div class="newsletter">
-      Anmeldung zum Newsletter
-      <form class="nf" @submit.prevent="subscribe" ref="form">
-        <span class="nf__at-sign">@: </span>
-        <input class="nf__email-input" type="email" placeholder="E-Mail-Adresse" @input="setEmail($event.target.value)" required />
-        <button class="nf__button button" type="submit">anmelden ⏎</button>
-      </form>
-      <div class="form-responses">
-        <div class="form-responses__success" v-if="success">Fast fertig.. Um die Anmeldung abzuschließen, klicken Sie bitte auf den Link in der E-Mail, die wir Ihnen gerade zugeschickt haben. Prüfen Sie ggf. auch Ihr Spam-Postfach.</div>
-        <div class="form-responses__error" v-if="error">Ein Fehler ist aufgetreten. Haben Sie sich eventuell vertippt oder sind Sie bereits im Verteiler?</div>
-        <div class="form-responses__loading" v-if="loading">lädt</div>
+    <template v-slot="{ subscribe, setEmail, error, success, loading }">
+      <div class="newsletter">
+        Anmeldung zum Newsletter
+        <form class="nf" @submit.prevent="subscribe" ref="form">
+          <span class="nf__at-sign">@: </span>
+          <input class="nf__email-input" type="email" placeholder="E-Mail-Adresse" @input="setEmail($event.target.value)" required />
+          <button class="nf__button button" type="submit">anmelden ⏎</button>
+        </form>
+        <div class="form-responses">
+          <div class="form-responses__success" v-if="success">Fast fertig.. Um die Anmeldung abzuschließen, klicken Sie bitte auf den Link in der E-Mail, die wir Ihnen gerade zugeschickt haben. Prüfen Sie ggf. auch Ihr Spam-Postfach.</div>
+          <div class="form-responses__error" v-if="error">Ein Fehler ist aufgetreten. Haben Sie sich eventuell vertippt oder sind Sie bereits im Verteiler?</div>
+          <div class="form-responses__loading" v-if="loading">lädt</div>
+        </div>
       </div>
-    </div>
-  </template>
-</mailchimp-subscribe>
+    </template>
+  </mailchimp-subscribe>
 </template>
 
 <script>
