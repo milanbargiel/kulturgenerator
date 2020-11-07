145ccdea6d34c6f41b53015c3ca5093e79c278e5
milanbargiel <milan.bargiel@googlemail.com>
2020-11-07 16:26:28 +0100

    [App] Add basic styling for thank you page

--- app/src/views/PurchaseConfirmation.vue
+++ app/src/views/PurchaseConfirmation.vue
@@ -1,10 +1,20 @@
 <template>
-    <div class="content">
-      <div class="text">
-            <span class="intext-title">Ihr Kauf war erfolgreich.</span>
+  <div class="content">
+    <div class="purchase text">
+      <div class="purchase__sucess">Herzlichen Glückwunsch, Ihr Kauf war erfolgreich!</div>
+      <div class="purchase__instructions">
+        <div class="purchase__img-container">
+          <img class="purchase__img" src="https://xyz.kulturgenerator.org/uploads/large_o_t_plasterpainting_rose_0_cd4ae43334.jpeg">
         </div>
-        <my-footer></my-footer>
+        <div class="purchase__message">
+          <p>1 Sie erhalten eine Bestätigungsmail von Paypal.</p>
+          <p>2 Die Anbieter*in nimmt Kontakt zu Ihnnen auf zur Vereeinbarung der Lieferung.</p>
+          <p>3 Das Kunstwerk kommt zu Ihnen.</p>
+        </div>
+      </div>
     </div>
+    <my-footer></my-footer>
+  </div>
 </template>
 
 <script>
