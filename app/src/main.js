6fa15b3e6cd662e22f8dd03b8485f94995ee8bce
milanbargiel <milan.bargiel@googlemail.com>
2020-11-09 10:41:17 +0100

    [App] Style desktop submit art form

--- app/src/main.js
+++ app/src/main.js
@@ -5,14 +5,9 @@ import store from './store'
 
 import VueImg from 'v-img' // full screen image slider
 import VueMarkdown from 'vue-markdown-v2'
-import VueFormulate from '@braid/vue-formulate'
-import { de } from '@braid/vue-formulate-i18n'
 
 Vue.use(VueImg) 
 Vue.component('vue-markdown', VueMarkdown)
-Vue.use(VueFormulate,  {
-  plugins: [ de ]
-})
 
 Vue.config.productionTip = false
 
