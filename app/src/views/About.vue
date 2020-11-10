d135f166aa66d2af79bf7ac677a7baa7e8250d05
katherina-gorod <71313926+katherina-gorod@users.noreply.github.com>
2020-11-10 08:44:01 +0100

    go

--- app/src/views/About.vue
+++ app/src/views/About.vue
@@ -6,17 +6,18 @@
             
 </div> 
 
-        <span class="about-title">SOLIDARITÄT</span><br>
-
-        Der Kulturgenerator ist eine Online-Galerie. Ziel ist es, Einnahmen durch Verkäufe unter allen teilnehmenden Kunst- und Kulturschaffenden fair umzuverteilen. Diese entscheiden dabei selbst, ob und wie viel sie vom Erlös des Verkaufs ihrer Arbeit auf ein solidarisches Konto einzahlen möchten und ob sie bei der Ausschüttung des Kontos beteiligt werden wollen. Auch teilnehmende Kunst- und Kulturschaffende, die über den Generator nichts verkaufen, können vom solidarischen Konto profitieren. Den Kontostand können Sie der Laufzeile im Shop entnehmen. Das Datum der Ausschüttung des solidarischen Kontos ist der 01.01.2021. <br><br><br><br>
-
-
-        
+<router-link class="submit-art-link title link" :to="{ name: 'submit' }"><img class="arrows-submit" src="@/assets/yellow-arrows.svg"></router-link><br><br>
+        <span class="yellow-numbers"> Über das <router-link class="submit-art-link underlined-link" :to="{ name: 'submit' }">Formular</router-link> können Künstlerinnen und Künstler und Kulturschaffende Arbeiten einreichen. Willkommen sind sowohl Objekte der bildenden Kunst, als auch kulturelle Angebote als Erlebnisse (soweit diese mit den aktuellen Bestimmungen realierbar sind) Es werden immer nur 30 Angebote gleichzeitig im Shop angezeigt, der Rest rückt nach, sobald etwas verkauft wird.</span><br><br><br><br>
 
         <span class="about-title">SICHTBARKEIT</span><br>
 
         Wenn der Strom ausfällt, wird ein Generator benötigt. Unter der Fragestellung, wie sich Kunst- und Kulturschaffende gegenseitig und eigenverantwortlich unterstützen können, ist der Kulturgenerator als Reaktion auf die COVID-19 Pandemie - vor allem in Betracht der Schließungen von Kulturinstitutionen in dem ersten Lockdown im März 2020 - entstanden.  Alle Angebote der Kunst- und Kulturschaffenden sind in ihm gleichberechtigt und hierarchiefrei nebeneinander positioniert, um gegenseitig die Sichtbarkeit zu erhöhen und sich zu unterstützen.
        <br><br><br><br>
+
+        <span class="about-title">SOLIDARITÄT</span><br>
+
+        Der Kulturgenerator ist eine Online-Galerie. Ziel ist es, Einnahmen durch Verkäufe unter allen teilnehmenden Kunst- und Kulturschaffenden fair umzuverteilen. Diese entscheiden dabei selbst, ob und wie viel sie vom Erlös des Verkaufs ihrer Arbeit auf ein solidarisches Konto einzahlen möchten und ob sie bei der Ausschüttung des Kontos beteiligt werden wollen. Auch teilnehmende Kunst- und Kulturschaffende, die über den Generator nichts verkaufen, können vom solidarischen Konto profitieren. Den Kontostand können Sie der Laufzeile im Shop entnehmen. Das Datum der Ausschüttung des solidarischen Kontos ist der 01.01.2021. <br><br><br><br>
+
 
         <span class="about-title">PLATTFORM FÜR KUNST UND KULTUR</span><br>
 
@@ -24,10 +25,9 @@
 
         <span class="about-title">SPIELTHEORETISCHES EXPERIMENT</span><br>
 
-        Wenn die Idee aufgeht, dann kann jede und jeder Teilnehmende mehr profitieren, wenn sie oder er bereit ist, etwas zu geben. Auch Sie können Teil unseres Projektes werden und Kulturschaffende unterstützen, indem Sie Angebote kaufen. Für Fragen schreiben Sie uns an info@kulturgenerator.org. <br><br><br><br>
+        Wenn die Idee aufgeht, dann kann jede und jeder Teilnehmende mehr profitieren, wenn sie oder er bereit ist, etwas zu geben. Auch Sie können Teil unseres Projektes werden und Kulturschaffende unterstützen, indem Sie Angebote kaufen. Für Fragen schreiben Sie uns an <a class="navigation-element link" href="mailto:info@kulturgenerator.org">info@kulturgenerator.org</a>. <br><br><br><br>
 
-        <router-link class="submit-art-link title link" :to="{ name: 'submit' }"><img class="arrows-submit" src="@/assets/yellow-arrows.svg"></router-link><br><br>
-        <span class="yellow-numbers"> Über das <router-link class="submit-art-link underlined-link" :to="{ name: 'submit' }">Formular</router-link> können Sie Arbeiten einreichen. Willkommen sind sowohl Objekte der bildenden Kunst, als auch kulturelle Angebote als Erlebnisse (soweit diese mit den aktuellen Bestimmungen realierbar sind) Es werden immer nur 30 Angebote gleichzeitig im Shop angezeigt, der Rest kann aber nachrücken, sobald etwas verkauft wird.</span><br><br><br><br>
+        
 
 
         
