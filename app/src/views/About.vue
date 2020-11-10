a29073d2134c51da081facea814eebe05510e643
katherina-gorod <71313926+katherina-gorod@users.noreply.github.com>
2020-11-10 01:43:03 +0100

    yes

--- app/src/views/About.vue
+++ app/src/views/About.vue
@@ -2,29 +2,35 @@
   <div class="content--blue">
     <div class="content">
       <div class="text">
-        <span class="about-title">STROM</span><br>
-
-        Wenn der Strom ausfällt, wird ein Generator benötigt. Der Kulturgenerator ist online seit XX Tagen, XX Stunden, XX Minuten. Als eine Reaktion auf die COVID-19 Pandemie, vor allem in Betracht der Schließungen von Kulturinstitutionen in dem ersten Lockdown im März 2020, ist der Kulturgenerator mit der Frage, wie sich Kunst- und Kulturschaffende gegenseitig und eigenverantwortlich unterstützen können, entstanden. In ihm erscheinen etablierte und weniger etablierte Künstler:innen gleichberechtigt und hierarchiefrei nebeneinander und können somit die gegenseitige Sichtbarkeit erhöhen und sich gegenseitig unterstützen.<br><br><br>
-
-
+         <div class="row">
+            
+</div> 
 
         <span class="about-title">SOLIDARITÄT</span><br>
 
-        Der Kulturgenerator ist somit eine solidarische Online-Galerie, die darauf abzielt, Einnahmen durch Verkäufe unter allen teilnehmenden Kunst- und Kulturschaffenden fair umzuverteilen. Diese entscheiden dabei selbst, ob und wie viel sie vom Erlös des Verkaufs ihrer Arbeit auf ein solidarisches Konto einzahlen möchten und ob sie bei der Ausschüttung des Kontos beteiligt werden wollen. Auch teilnehmende Kunst- und Kulturschaffende, die über den Generator nichts verkaufen, können vom solidarischen Konto profitieren. Der Kontostand beträgt derzeit: XXX Euro. Das Datum der Ausschüttung des solidarischen Kontos ist der XX/XX/XX Datum.<br><br><br><br>
+        Der Kulturgenerator ist eine Online-Galerie. Ziel ist es, Einnahmen durch Verkäufe unter allen teilnehmenden Kunst- und Kulturschaffenden fair umzuverteilen. Diese entscheiden dabei selbst, ob und wie viel sie vom Erlös des Verkaufs ihrer Arbeit auf ein solidarisches Konto einzahlen möchten und ob sie bei der Ausschüttung des Kontos beteiligt werden wollen. Auch teilnehmende Kunst- und Kulturschaffende, die über den Generator nichts verkaufen, können vom solidarischen Konto profitieren. Den Kontostand können Sie der Laufzeile im Shop entnehmen. Das Datum der Ausschüttung des solidarischen Kontos ist der 01.01.2021. <br><br><br><br>
 
 
+        
 
-        <span class="about-title">SPARTENÜBERGREIFENDE PLATTFORM</span><br>
+        <span class="about-title">SICHTBARKEIT</span><br>
 
-        Da vor allem auch kulturorganisatorische Arbeit und Kultureinrichtungen der freien Szene oft aus dem Raster der Unterstützungsmaßnahmen fallen, können im Generator neben Objekten auch Kultur-Erlebnisse verkauft werden. Seit seinem bestehen hat der Kulturgenerator bereits XX Arbeiten generiert. Im Laden stehen 30 davon zum Verkauf. Wird ein Angebot verkauft, rückt ein anderes nach.<br><br><br><br>
+        Wenn der Strom ausfällt, wird ein Generator benötigt. Unter der Fragestellung, wie sich Kunst- und Kulturschaffende gegenseitig und eigenverantwortlich unterstützen können, ist der Kulturgenerator als Reaktion auf die COVID-19 Pandemie - vor allem in Betracht der Schließungen von Kulturinstitutionen in dem ersten Lockdown im März 2020 - entstanden.  Alle Angebote der Kunst- und Kulturschaffenden sind in ihm gleichberechtigt und hierarchiefrei nebeneinander positioniert, um gegenseitig die Sichtbarkeit zu erhöhen und sich zu unterstützen.
+       <br><br><br><br>
 
+        <span class="about-title">PLATTFORM FÜR KUNST UND KULTUR</span><br>
 
+        Da vor allem auch kulturorganisatorische Arbeit und Kultureinrichtungen der freien Szene oft aus dem Raster der Unterstützungsmaßnahmen fallen, können im Generator neben Objekten auch Kultur-Erlebnisse verkauft werden. Seit seinem bestehen hat der Kulturgenerator bereits 53 Arbeiten generiert. Im Laden stehen 30 davon zum Verkauf. Wird ein Angebot verkauft, rückt ein anderes nach.<br><br><br><br>
 
-        <span class="about-title">SPIEL</span><br>
+        <span class="about-title">SPIELTHEORETISCHES EXPERIMENT</span><br>
 
-        Sie können Teil unseres Experiments werden, indem Sie Angebote kaufen oder Arbeiten über das Formular einreichen. Für Fragen schreiben Sie uns an info@kulturgenerator.org oder melden Sie sich für unseren Newsletter an: <br><br><br><br>
+        Wenn die Idee aufgeht, dann kann jede und jeder Teilnehmende mehr profitieren, wenn sie oder er bereit ist, etwas zu geben. Auch Sie können Teil unseres Projektes werden und Kulturschaffende unterstützen, indem Sie Angebote kaufen. Für Fragen schreiben Sie uns an info@kulturgenerator.org. <br><br><br><br>
 
         <router-link class="submit-art-link title link" :to="{ name: 'submit' }"><img class="arrows-submit" src="@/assets/yellow-arrows.svg"></router-link><br><br>
+        <span class="yellow-numbers"> Über das <router-link class="submit-art-link underlined-link" :to="{ name: 'submit' }">Formular</router-link> können Sie Arbeiten einreichen. Willkommen sind sowohl Objekte der bildenden Kunst, als auch kulturelle Angebote als Erlebnisse (soweit diese mit den aktuellen Bestimmungen realierbar sind) Es werden immer nur 30 Angebote gleichzeitig im Shop angezeigt, der Rest kann aber nachrücken, sobald etwas verkauft wird.</span><br><br><br><br>
+
+
+        
       
 
       
@@ -50,3 +56,4 @@
     }
   }
 </script>
+
