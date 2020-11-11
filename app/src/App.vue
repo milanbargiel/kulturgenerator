a36903fc1b75c75aceaadc23bb229fe30544ed51
milanbargiel <milan.bargiel@googlemail.com>
2020-11-11 16:37:48 +0100

    [App] Fix minor typos

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div v-if="isLoading" class="loading-indicator">lädt</div>
     <div :class="{ 'hidden': isLoading }">
-      <div v-if="isArtworkList" class="info-box">Der <span class="intext-title">kulturgenerator</span> ist eine solidarische Online-Galerie auf Initiative des <a class="link" href="https://www.koelnerkulturrat.de/" target="_blank">Kölner Kulturrats</a>, der <a class="link" href="https://www.niehlerfreiheit.de/" target="_blank">Niehler Freiheit e.V.</a>und des<a class="link" href="https://kunstzentrum-wachsfabrik.de/" target="_blank"> Kunstzentrum Wachsfabrik.</a> Alle Teilnehmenden Kunst- und Kulturschaffenden entscheiden selbst, ob und in welchem Umfang ihre Erlöse auf ein solidarisches Konto eingezahlt werden. Für weitere Informationen klicken Sie auf die Laufschrift, die den aktuellen Kontostand anzeigt ↓</div>
+      <div v-if="isArtworkList" class="info-box">Der <span class="intext-title">kulturgenerator</span> ist eine solidarische Online-Galerie auf Initiative des <a class="link" href="https://www.koelnerkulturrat.de/" target="_blank">Kölner Kulturrats</a>, der <a class="link" href="https://www.niehlerfreiheit.de/" target="_blank">Niehler Freiheit e.V.</a>und des<a class="link" href="https://kunstzentrum-wachsfabrik.de/" target="_blank"> Kunstzentrum Wachsfabrik.</a> Alle teilnehmende Kunst- und Kulturschaffende entscheiden selbst, ob und in welchem Umfang ihre Erlöse auf ein solidarisches Konto eingezahlt werden. Für weitere Informationen klicken Sie auf die Laufschrift, die den aktuellen Kontostand anzeigt ↓</div>
       <my-header :isLoading="isLoading"></my-header>
       <router-view/> 
     </div>
