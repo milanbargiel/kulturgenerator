af9e470e9ec9786ff6a919b61757b8a4b1dc51a4
milanbargiel <milan.bargiel@googlemail.com>
2020-11-30 09:49:32 +0100

    [App] Add noopener attribute to footer links

--- app/src/components/Footer.vue
+++ app/src/components/Footer.vue
@@ -3,25 +3,25 @@
     <footer class="footer">
       <div class="navigation">
         <a :class="navigationElementClass" href="mailto:info@kulturgenerator.org"><span :class="underlinedLinkClass">info@kulturgenerator.org</span></a><br>
-        <a :class="navigationElementClass" href="https://www.instagram.com/kulturgenerator" target="_blank"><span :class="underlinedLinkClass">Instagram</span></a><br>
+        <a :class="navigationElementClass" href="https://www.instagram.com/kulturgenerator" target="_blank" rel="noopener"><span :class="underlinedLinkClass">Instagram</span></a><br>
         <router-link :to="{name: 'impressum'}" :class="navigationElementClass"><span :class="underlinedLinkClass">Impressum + Datenschutz</span></router-link>
       </div>
       <div class="founders">
         <div class="founders__logos">
-          <a class="founder-link founder-link--kk" href="https://www.koelnerkulturrat.de/" target="_blank">
+          <a class="founder-link founder-link--kk" href="https://www.koelnerkulturrat.de/" target="_blank" rel="noopener">
             <img class="founder-img" :src="kulturratLogo">
           </a>
-          <a class="founder-link founder-link--wf" href="https://kunstzentrum-wachsfabrik.de/" target="_blank">
+          <a class="founder-link founder-link--wf" href="https://kunstzentrum-wachsfabrik.de/" target="_blank" rel="noopener">
             <img class="founder-img" :src="wachsfabrikLogo">
           </a>
-          <a class="founder-link founder-link--nf" href="https://niehlerfreiheit.de/" target="_blank">
+          <a class="founder-link founder-link--nf" href="https://niehlerfreiheit.de/" target="_blank" rel="noopener">
             <img class="founder-img" :src="niehlerfreiheitLogo">
           </a>
           <span class="breaker"></span>
-          <a class="founder-link founder-link--sponsors" href="https://www.stadt-koeln.de/" target="_blank">
+          <a class="founder-link founder-link--sponsors" href="https://www.stadt-koeln.de/" target="_blank" rel="noopener">
             <img class="founder-img" :src="kulturamtLogo">
           </a>
-          <a class="founder-link founder-link--sponsors" href="https://www.rheinenergiestiftung.de/de/kultur/index.php" target="_blank">
+          <a class="founder-link founder-link--sponsors" href="https://www.rheinenergiestiftung.de/de/kultur/index.php" target="_blank" rel="noopener">
             <img class="founder-img" :src="rheinenergieLogo">
           </a>          
         </div>
