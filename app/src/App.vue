adf7a077a7ac1241c5ed40a9cabaf257e0267de2
milanbargiel <milan.bargiel@googlemail.com>
2021-01-23 15:39:15 +0100

    [App] Add hourglass

--- app/src/App.vue
+++ app/src/App.vue
@@ -3,6 +3,7 @@
     <div v-if="isLoading" class="loading-indicator">lädt</div>
     <div :class="{ 'hidden': isLoading }">
       <div v-if="isArtworkList" class="info-box">In <countdown></countdown> ist die letzte Möglichkeit, Arbeiten aus der ersten Runde des <span class="intext-title">kulturgenerators</span> zu erwerben und somit das solidarische Konto aufzufüllen. Diese Online-Galerie ist ein Projekt auf Initiative des Kölner Kulturrats, der Niehler Freiheit e.V. und des Kunstzentrum Wachsfabrik. Für weitere Informationen klicken Sie auf die Laufleiste, die den aktuellen Kontostand anzeigt ↓</div>
+      <hourglass v-if="isArtworkList"></hourglass>
       <my-header :isLoading="isLoading"></my-header>
       <router-view/> 
     </div>
@@ -13,10 +14,11 @@
 import { mapGetters } from 'vuex'
 import MyHeader from './components/Header'
 import Countdown from './components/Countdown'
+import Hourglass from './components/Hourglass'
 
 export default {
   name: 'App',
-  components: { MyHeader, Countdown },
+  components: { MyHeader, Countdown, Hourglass },
   created () {
     this.$store.dispatch('getShadowMoneypoolBalance')
     this.$store.dispatch('getMenuItems')
---
+++ app/src/components/Hourglass.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="hourglass">
+    <div class="top"></div>
+    <div class="bottom"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hourglass',
+}
+</script>
