786bf87a88840542a5ec13371324fc02b13ba007
milanbargiel <milan.bargiel@googlemail.com>
2021-01-30 17:47:14 +0100

    [App] Place archive information

--- app/src/views/Archive.vue
+++ app/src/views/Archive.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <div class="archive-info text">In diesem Archiv finden Sie Arbeiten aus der ersten Runde des <span class="intext-title">kulturgenerators</span>. Die Runde lief vom 18. November 2020 bis zum 01. Februar 2021 und es wurden <span class="highlighted-text">7025€</span> für ein solidarisches Konto generiert, dass am Ende an alle teilnehmenden Künstler:innen ausgeschüttet wurde.</div>
     <artwork-list :artworks="artworks" :is-archive="true"></artwork-list>
     <my-footer></my-footer>
   </div>
