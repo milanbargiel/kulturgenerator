95f2de196a45faaa99655e21f960d09817cd80b6
milanbargiel <milan.bargiel@googlemail.com>
2021-02-01 09:05:28 +0100

    [App] Changes to archive text

--- app/src/views/Archive.vue
+++ app/src/views/Archive.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="content text">Hier finden Sie alle Arbeiten aus der ersten Runde des <span class="intext-title">kulturgenerators</span>, einer solidarischen Online-Galerie auf Initiative des Kölner Kulturrats, der Niehler Freiheit e.V. und des Kunstzentrum Wachsfabrik. Vom 18. November 2020 bis zum 01. Februar 2021 wurden <span class="highlighted-text">7025€</span> für ein solidarisches Konto generiert, dass nun an alle teilnehmenden Künstler:innen anteilig ausgeschüttet wird.</div>
+    <div class="content text text--blue">Hier finden Sie alle Arbeiten aus der ersten Runde des <span class="intext-title">kulturgenerators</span>, einer solidarischen Online-Galerie auf Initiative des Kölner Kulturrats, der Niehler Freiheit e.V. und des Kunstzentrum Wachsfabrik. Vom 18. November 2020 bis zum 01. Februar 2021 wurden <span class="highlighted-text">7609,86€</span> für ein solidarisches Konto generiert, dass nun an alle teilnehmenden Künstler*innen anteilig ausgeschüttet wird.</div>
     <artwork-list :artworks="artworks" :is-archive="true"></artwork-list>
     <my-footer></my-footer>
   </div>
