fc9a06815ef8a89be5a5ee99e4ee0aaa1be2d40d
milanbargiel <milan.bargiel@googlemail.com>
2021-02-15 12:19:44 +0100

    [CMS] Return round information also for single views

--- cms/api/artwork/controllers/artwork.js
+++ cms/api/artwork/controllers/artwork.js
@@ -9,9 +9,15 @@ module.exports = {
 
   async findOne(ctx) {
     const { slug } = ctx.params;
-
     const entity = await strapi.services.artwork.findOne({ slug });
-    return sanitizeEntity(entity, { model: strapi.models.artwork });
+    const artwork = sanitizeEntity(entity, { model: strapi.models.artwork });
+
+    // Return round information even though status is private
+    if (entity.status === 'ZweiteRunde' || entity.status === 'ErsteRunde') {
+      artwork.status =  entity.status; 
+    }
+
+    return artwork;
   },
   async find(ctx) {
     let entities;
