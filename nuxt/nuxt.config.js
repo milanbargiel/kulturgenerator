678ac2698b38044f62ced1035857a76ed6b066a2
Moritz Pflüger <m.pflueger@meta-fusion.com>
2021-03-19 13:59:10 +0100

    [Nuxt] try fixing failing build setting shared.scss in nuxt config

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -17,7 +17,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['~/assets/styles/App.scss'],
+  css: ['~/assets/styles/App.scss', '~/assets/styles/shared.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
