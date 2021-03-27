7f65bd18d0266ab489b0461027f42fd8d0790191
Moritz Pflüger <m.pflueger@meta-fusion.com>
2021-03-27 12:08:55 +0100

    [Nuxt] update global meta tags

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -4,14 +4,23 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxt2',
+    title: 'Kulturgenerator | Online-Galerie',
     htmlAttrs: {
       lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Eine generöse Maschine zur finanziellen Unterstützung von Kölner Kulturschaffenden während der Coronakrise',
+      },
+      {
+        name: 'keywords',
+        content: 'Kunst, Köln, Solidarität, Corona, Kulturhilfe, Kulturrat',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
