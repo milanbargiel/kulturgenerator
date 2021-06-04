18b5ef2580147fea253e800513b97cd6f1341f3b
milanbargiel <milan.bargiel@googlemail.com>
2021-06-04 11:16:30 +0200

    [App] Add hourglass to construction page

--- app/src/views/UnderConstruction.vue
+++ app/src/views/UnderConstruction.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <div class="hourglass">
+    <div class="content text">
+      Hier sehen sie alle Arbeiten, die an der ersten Runde des <span class="intext-title intext-title--blue">kulturgenerators</span> beteiligt waren. Sie wurde am 01.02.2021 mit der Ausschüttung des solidarischen Kontos in Höhe von 7766,50 EUR durch den Verkauf von 38 Arbeiten abgeschlossen. Für die 50 Teilnehmenden be- deutet dies, dass jede*r einen Betrag von 153,41 EUR erhalten hat. Wenn Sie Arbeiten davon kaufen wollen, treten Sie bitte mit uns in Kontakt.</div>
+      <div class="content hourglass hourglass--construction">
       <div class="top"></div>
       <div class="bottom"></div>
     </div>
-    <div class="content text">Hier sehen sie alle Arbeiten, die an der ersten Runde des <span class="intext-title">kulturgenerators</span> beteiligt waren. Sie wurde am 01.02.2021 mit der Ausschüttung des solidarischen Kontos in Höhe von 7766,50 EUR durch den Verkauf von 38 Arbeiten abgeschlossen. Für die 50 Teilnehmenden be- deutet dies, dass jede*r einen Betrag von 153,41 EUR erhalten hat. Wenn Sie Arbeiten davon kaufen wollen, treten Sie bitte mit uns in Kontakt.</div>
-    <artwork-list :artworks="artworks" :is-archive="true"></artwork-list>
     <my-footer></my-footer>
   </div>
 </template>
