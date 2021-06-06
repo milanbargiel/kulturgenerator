972f4e38bb97f7b842490f26b391dba2ff664613
milanbargiel <milan.bargiel@googlemail.com>
2021-06-06 12:47:13 +0200

    [App] Change text on under-construction page

--- app/src/views/UnderConstruction.vue
+++ app/src/views/UnderConstruction.vue
@@ -5,10 +5,8 @@
         <div class="top"></div>
         <div class="bottom"></div>
       </div>
-              
-
       <div>
-      Wie kann Solidarität innerhalb der Kunst- und Kulturszene aussehen? Der <span class="intext-title intext-title--blue">kulturgenerator</span> ist eine solidarische Online-Galerie auf Initiative des Kölner Kulturrats, der Niehler Freiheit e.V. und des Kunstzentrum Wachsfabrik. Alle Teilnehmenden entscheiden selbst, ob und in welchem Umfang ihre Erlöse solidarisch verteilt werden. <span class="highlighted-text">Diese Seite befindet sich im Umbau.</span> Tragen Sie sich im Newsletter ein, um über Neuigkeiten auf dem Laufenden gehalten zu werden.</div></div>
+      Wie kann Solidarität innerhalb der Kunst- und Kulturszene aussehen? Der <span class="intext-title intext-title--blue">kulturgenerator</span> ist eine solidarische Online-Galerie auf Initiative des Kölner Kulturrats, der Niehler Freiheit e.V. und des Kunstzentrum Wachsfabrik. Teilnehmende Kunst- und Kulturschaffenden entscheiden selbst, ob und wie viel ihrer Erlöse durch Verkauf auf ein solidarisches Konto eingezahlt werden, das am Ende unter allen ausgeschüttet wird.<span class="highlighted-text">Diese Seite befindet sich im Umbau.</span> Tragen Sie sich im Newsletter ein, um über Neuigkeiten auf dem Laufenden gehalten zu werden.</div></div>
     <my-footer></my-footer>
   </div>
 </template>
